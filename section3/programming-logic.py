3b3776685aff3e1a1fa1cc6fbe291236483e0660
Luiz Marques <l.fernando.9.lfsm@gmail.com>
2025-04-28 01:19:13 -0300

    Starting in programming logic

---
+++ section3/programming-logic.py
@@ -0,0 +1,60 @@
+#Code comment
+print(123) #comment
+
+"""
+Docstrings
+with double quotes
+The interpreter read, but it does not run; keep only in memory.
+"""
+
+'''
+Docstrings
+with single quotes
+The interpreter read, but it does not run; keep only in memory.
+'''
+
+#Print function
+#It is used to show something on the screen/terminal.
+#Unnamed and named arguments
+print(25,45, sep="-",end="##\r\n")
+print(35,45,sep='-',end='\n')
+print('Hello world')
+
+#Obs.: Python is dinamically and strongly typed.
+
+#Data type: string
+#It is used to represent text.
+print('Luiz')
+print("Luiz")
+print("Luiz \"Fernando\"")
+print(r"Luiz \"Fernando\"")
+print("Luiz 'Fernando'")
+print('Luiz "Fernando"')
+
+#Data type: int
+#It is used to represent integers.
+print(1)
+print(-1)
+print(0)
+
+#Data type: float
+#It is used to represent decimal numbers (Floating point).
+print(1.0)
+print(-1.0)
+print(0.0)
+
+#Type class
+#It is used to show, the data type.
+print(type(1))
+print(type('Luiz'))
+print(type(2.0),type(-2.0),type(0.0))
+
+#Data type: bool
+#It represents the value True or False.
+print(True)
+print(False)
+print(10==10)
+print(10==11)
+print(type(True), type(False), type(10==10))
+
+
