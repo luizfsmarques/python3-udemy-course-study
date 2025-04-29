715f604087082c6c025232e75880bf8b044fdbfa
Luiz Marques <l.fernando.9.lfsm@gmail.com>
2025-04-29 15:38:01 -0300

    Class about arithmetic operators.

--- section3/programming-logic.py
+++ section3/programming-logic.py
@@ -1,5 +1,5 @@
-#Code comment
-print(123) #comment
+# Code comment
+print(123) # comment
 
 """
 Docstrings
@@ -13,17 +13,17 @@ with single quotes
 The interpreter read, but it does not run; keep only in memory.
 '''
 
-#Print function
-#It is used to show something on the screen/terminal.
-#Unnamed and named arguments
+# Print function
+# It is used to show something on the screen/terminal.
+# Unnamed and named arguments
 print(25,45, sep="-",end="##\r\n")
 print(35,45,sep='-',end='\n')
 print('Hello world')
 
-#Obs.: Python is dinamically and strongly typed.
+# Obs.: Python is dinamically and strongly typed.
 
-#Data type: string
-#It is used to represent text.
+# Data type: string
+# It is used to represent text.
 print('Luiz')
 print("Luiz")
 print("Luiz \"Fernando\"")
@@ -31,40 +31,45 @@ print(r"Luiz \"Fernando\"")
 print("Luiz 'Fernando'")
 print('Luiz "Fernando"')
 
-#Data type: int
-#It is used to represent integers.
+# Data type: int
+# It is used to represent integers.
 print(1)
 print(-1)
 print(0)
 
-#Data type: float
-#It is used to represent decimal numbers (Floating point).
+# Data type: float
+# It is used to represent decimal numbers (Floating point).
 print(1.0)
 print(-1.0)
 print(0.0)
 
-#Type class
-#It is used to show, the data type.
+# Type class
+# It is used to show, the data type.
 print(type(1))
 print(type('Luiz'))
 print(type(2.0),type(-2.0),type(0.0))
 
-#Data type: bool
-#It represents the value True or False.
+# Data type: bool
+# It represents the value True or False.
 print(True)
 print(False)
 print(10==10)
 print(10==11)
 print(type(True), type(False), type(10==10))
 
-#Variables
-#It is used to represent memory spaces and its values.
+# Variables
+# It is used to represent memory spaces and its values.
 full_name = 'Luiz Marques'
 age = 30
 of_legal_age = age >= 18
 print('Name: ',full_name,'Age: ',age)
 print('of_legal_age?',of_legal_age)
 
-
-
-
+# Arithmetic operators
+# It is used to execute some operation
+addition = 10 + 10
+subtraction = 10 -5
+multiplication = 10 * 10 
+floor_division = 10 // 3
+exponentiation = 2 ** 10
+modulus = 10 % 2
